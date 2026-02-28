1d685ae0371a85a53dffbb1f45f065dd9d0498c7
Jimmy McGill <256928395+minigame-labs@users.noreply.github.com>
2026-02-28 17:10:16 +0800

    feat(console): enhance logging methods with argument formatting

--- engine/crates/js-runtime/console/01_console.js
+++ engine/crates/js-runtime/console/01_console.js
@@ -1,20 +1,38 @@
 import { op_console } from "ext:core/ops";
 
+let groupDepth = 0;
+
+function formatArgs(args) {
+    const indent = "  ".repeat(groupDepth);
+    const msg = args.map((a) => {
+        if (typeof a === "string") return a;
+        try { return JSON.stringify(a); } catch (_) { return String(a); }
+    }).join(" ");
+    return indent + msg;
+}
+
 class Console {
+    debug(...args) {
+        op_console(formatArgs(args), 0);
+    }
     log(...args) {
-        op_console(args, 1);
-    }
-    warn(...args) {
-        op_console(args, 2);
-    }
-    error(...args) {
-        op_console(args, 3);
+        op_console(formatArgs(args), 1);
     }
     info(...args) {
-        op_console(args, 1);
+        op_console(formatArgs(args), 1);
     }
-    debug(...args) {
-        op_console(args, 1);
+    warn(...args) {
+        op_console(formatArgs(args), 2);
+    }
+    error(...args) {
+        op_console(formatArgs(args), 3);
+    }
+    group(label) {
+        op_console("  ".repeat(groupDepth) + (label ?? ""), 1);
+        groupDepth++;
+    }
+    groupEnd() {
+        if (groupDepth > 0) groupDepth--;
     }
 }
 
